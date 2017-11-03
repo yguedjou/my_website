093931297bcce36b6d4789f1abda45cb525aa4a4
Yasmine <yasmine.guedjou@hotmail.fr>
2017-11-03 12:54:07 +0100

    add index.php

---
+++ index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+
+<html lang="fr">
+	<head>
+		<title>Projet - WIP</title>
+		<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+		<link href="./css/style.css" rel="stylesheet" type="text/css">
+		<link href="./css/icon.css" rel="stylesheet">
+		<link rel="shortcut icon" type="image/png" href="img/wheel_v1.4.png"/>
+		<style type="text/css">
+			h1 {
+				font-size: 22pt;
+				padding: 30px 45%;
+			}
+			h2 {
+				font-size: 18pt;
+				margin: 0px 20px;
+				padding: 10px 20px;
+			}
+			p.text {
+				font-size: 12pt;
+				padding: 0em 3em;
+			}
+		</style>
+	</head>
+	
+	<body>
+		<nav>
+			<ul>
+				<li class="sliding-middle-out" style="margin-left: 13%;">					
+					<a href=""><i class="icon-home"></i>Accueil</a>
+				</li>
+				<li class="sliding-middle-out">
+					<a href="./project/index.php"><i class="icon-project"></i>Projet</a>
+				</li>
+				<li class="sliding-middle-out">
+					<a href="./training/tramis/index.html"><i class="icon-tutorial"></i>Travaux Dirig&eacute;s</a>
+				</li>
+				<li class="bikelogo">
+					<figure style="	margin-top: 4px; ">
+						<img src="./img/home_icon.png" alt="home" style="width:100px; height:40px;">
+					<figcaption class="hidden">Bike</figcaption>
+					</figure>
+				</li>
+			</ul>
+		</nav>
+		
+		<section>
+			<h1>Bienvenue</h1>
+			<p class="text">Tous les entra&icirc;nements de l'UE D&eacute;veloppement Web, introduit au 4<sup>&egrave;me</sup> semestre
+			se trouvent ici, ainsi que le projet final de 2017.</p> 
+			<article>
+				<h2 style="border-bottom: 2px solid #666699">Le projet</h2>
+				<p class="text">Rechercher des informations sur un &eacute;tablissement d'enseignement sup&eacute;rieur pr&eacute;cis,
+				 ou afficher diff&eacute;rents &eacute;tablissement en fonction d'un seul crit&egrave;re.
+				 Pr&eacute;sentation sous forme de graphe &eacute;galement disponible.</p>
+				<p class="text">Aller au projet :</p>
+				<a class="button-project" href="project/index.php"><i class="icon-project"></i>Projet</a>
+							
+			</article>
+			
+			<article>
+				<h2 style="border-bottom: 2px solid #ffa366">Les travaux dirig&eacute;s</h2>
+				<p class="text">Liens vers les TD des 2 membres de l'&eacute;quipe.</p>
+				<p class="text">Acc&eacute;der aux TD :</p>
+				<a class="button-tramis" href="training/tramis/index.html"><i class="icon-tutorial"></i>Lo&iuml;c T.</a>				
+				<a class="button-noname" href="training/tramis/index.html"><i class="icon-tutorial"></i>Noname</a>
+			</article>
+		</section>
+	
+		<footer>
+		
+		</footer>
+	
+	</body>
+
+</html>
